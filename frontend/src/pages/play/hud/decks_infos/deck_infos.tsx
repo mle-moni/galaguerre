83ac8cc304f08af45845844e2ede06909dd67ae9
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2024-08-18 18:25:13 +0200

    feat: better deck infos

--- frontend/src/pages/play/hud/decks_infos/deck_infos.tsx
+++ frontend/src/pages/play/hud/decks_infos/deck_infos.tsx
@@ -1,5 +1,5 @@
 import type { GamePlayer } from "#api_types/game.types";
-import { Text } from "@mantine/core";
+import { Button } from "@mantine/core";
 import { IconPlayCard } from "@tabler/icons-react";
 
 interface DeckInfosProps {
@@ -7,28 +7,36 @@ interface DeckInfosProps {
     isOpponent?: boolean;
 }
 
-export const DeckInfos = ({ player }: DeckInfosProps) => {
+export const DeckInfos = ({ player, isOpponent }: DeckInfosProps) => {
     const numberOfCards = player.deckCards.length;
     const iconSize = getIconSize(numberOfCards);
 
     return (
-        <div>
-            <div className="flex h-[80px] justify-between items-center">
-                <Text className="flex-1" size="xl" ta="center">
-                    {numberOfCards}
-                </Text>
+        <div className="flex flex-col w-full">
+            {isOpponent && <div className="h-[80px]" />}
+            <div className="flex h-[80px] items-center">
+                <p className="flex-1 mr-2 text-right text-xl">{numberOfCards}</p>
                 <IconPlayCard className="flex-1" size={iconSize} />
             </div>
+            {!isOpponent && (
+                <div className="h-[80px]">
+                    <Button variant="filled">Terminé</Button>
+                </div>
+            )}
         </div>
     );
 };
 
-const getIconSize = (numberOfCards: number) => {
-    // NUM_CARDS_MIN 0 -> SIZE_MIN 40
-    // NUM_CARDS_MAX 30 -> SIZE_MAX 80
-    // SIZE_X = (4 * NC_X) / 3 + 40
-    const ncX = numberOfCards > 30 ? 30 : numberOfCards;
-    const sizeX = (4 * ncX) / 3 + 40;
+const getIconSize = (numCards: number): number => {
+    const NUM_CARDS_MIN = 0;
+    const NUM_CARDS_MAX = 30;
+    const MIN_SIZE = 22;
+    const MAX_SIZE = 50;
 
-    return sizeX;
+    const numCardOrMax = numCards > NUM_CARDS_MAX ? NUM_CARDS_MAX : numCards;
+
+    return (
+        MIN_SIZE +
+        ((numCardOrMax - NUM_CARDS_MIN) / (NUM_CARDS_MAX - NUM_CARDS_MIN)) * (MAX_SIZE - MIN_SIZE)
+    );
 };
