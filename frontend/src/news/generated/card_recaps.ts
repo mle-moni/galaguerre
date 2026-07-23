611de62acc37a557ed3d05be1853b512d1d919a7
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2026-07-23 22:06:43 +0200

    feat: more gold

--- frontend/src/news/generated/card_recaps.ts
+++ frontend/src/news/generated/card_recaps.ts
@@ -430,4 +430,18 @@ export const CARD_RECAPS: CardRecapData[] = [
         nerfs: [],
         imageUrl: "/card-covers/galadrim/stagiaire-planque.webp",
     },
+    {
+        date: "2026-07-23",
+        slug: "recap-2026-07-23",
+        publishedAt: "2026-07-23T18:00:00+02:00",
+        untilCommitHash: "1cd613b268b4e1c6eb77f2d3cf63d1f5273d716e",
+        newCardIds: [],
+        newGoldenCardIds: [
+            65, 66, 174, 72, 78, 83, 131, 87, 88, 89, 94, 154, 156, 111, 161, 116, 118, 119, 96, 98,
+            101, 102, 132, 120, 127, 175, 107, 171,
+        ],
+        buffs: [],
+        nerfs: [],
+        imageUrl: "/card-covers/galadrim/dev-aigri.webp",
+    },
 ];
